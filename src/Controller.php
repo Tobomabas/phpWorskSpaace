3f115255261af10c3033000a77ed40fb5d802a23
Tomas <tobomabas@gmail.com>
2022-12-18 19:27:39 +0100

    q

--- src/Controller.php
+++ src/Controller.php
@@ -21,7 +21,6 @@ class Controller
          
         return $this->getRequestGet()['action'] ?? self::DEFAUL_ACTION;
     }
-
         public function run(): void{
        
                  $viewParams = [];
