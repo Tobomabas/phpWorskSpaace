5c91c934b0010d6fa703d91cc413cbe024a41e3d
Tomas <tobomabas@gmail.com>
2022-12-18 18:29:05 +0100

    v

--- index.php
+++ index.php
@@ -2,18 +2,14 @@
 
 declare(strict_types=1);
 namespace App;
- 
 
 require_once("src/utils/debug.php");
 require_once("src/Controller.php");
-
 
 $request = [
     'get' => $_GET,
     'post' => $_POST
 ];
-
-
 
 // $controller = new Controller($request);
 // $controller->run();
